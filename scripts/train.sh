eeab6ad8de0777cdb81e3d50cd807f52b2676a36
Patrickwhj <huw011@eng.ucsd.edu>
2021-06-26 20:28:12 +0000

    fix train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Dataset:" $1  "Backbone:" $2  "GPU index:" $3 "Tag:" $4 
 
-python train_classifier_sideout_classifier_ybranch.py --gpu $3 --config ./configs/$1/$2-ybranch-cifar-fs.yaml --tag=$4
+python train_classifier_sideout_classifier_ybranch.py --gpu $3 --config ./configs/$1/$2-ybranch-$1.yaml --tag=$4
