4a41a09810e1d330dae65ae1424bec288433c788
MrMeepsle <mynameismailtje@gmail.com>
2022-03-24 10:12:19 +0100

    Pickled all photos

--- pickler.py
+++ pickler.py
@@ -3,15 +3,25 @@ from PIL import Image
 import pickle
 import os
 
-# load the image
-img_dir = 'raw-pacs/pacs_data/pacs_data/photo/dog/'
+img_dir = 'raw-pacs/pacs_data/pacs_data/photo/'
+classes_dict = {
+    "dog": 1,
+    "elephant": 2,
+    "giraffe": 3,
+    "guitar": 4,
+    "horse": 5,
+    "house": 6,
+    "person": 7,
+}
 data = []
 labels = []
 
-for filename in os.listdir(img_dir):
-    if filename.endswith((".jpg", ".png")):
-        data.append(np.asarray(Image.open(img_dir + filename)))
-        labels.append(1)
+print("Start pickling classes")
+for class_ in classes_dict:
+    for filename in os.listdir(img_dir + class_):
+        if filename.endswith((".jpg", ".png")):
+            data.append(np.asarray(Image.open(img_dir + class_ + "/" + filename)))
+            labels.append(classes_dict[class_])
 
 # Create a dict for pickling
 pickle_dict = {
@@ -22,3 +32,4 @@ pickle_dict = {
 pickle.dump(pickle_dict, open("materials/cifar-fs/CIFAR_FS_test.pickle", "wb"))
 pickle.dump(pickle_dict, open("materials/cifar-fs/CIFAR_FS_train.pickle", "wb"))
 pickle.dump(pickle_dict, open("materials/cifar-fs/CIFAR_FS_val.pickle", "wb"))
+print("Pickling finished")
