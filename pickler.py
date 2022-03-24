93b6874510cbe5800d235b192df04e9fafddd5d0
MrMeepsle <mynameismailtje@gmail.com>
2022-03-24 09:55:10 +0100

    Pickled all dog photos

--- pickler.py
+++ pickler.py
@@ -1,15 +1,24 @@
 import numpy as np
 from PIL import Image
 import pickle
+import os
 
 # load the image
-image = Image.open('raw-pacs/pacs_data/pacs_data/photo/dog/056_0001.jpg')
-# convert image to numpy array
-picledict = {
-    "data": [np.asarray(image), np.asarray(image)],
-    "labels": [1, 2],
+img_dir = 'raw-pacs/pacs_data/pacs_data/photo/dog/'
+data = []
+labels = []
+
+for filename in os.listdir(img_dir):
+    if filename.endswith((".jpg", ".png")):
+        data.append(np.asarray(Image.open(img_dir + filename)))
+        labels.append(1)
+
+# Create a dict for pickling
+pickle_dict = {
+    "data": data,
+    "labels": labels,
 }
 
-pickle.dump(picledict, open("materials/cifar-fs/CIFAR_FS_test.pickle", "wb"))
-pickle.dump(picledict, open("materials/cifar-fs/CIFAR_FS_train.pickle", "wb"))
-pickle.dump(picledict, open("materials/cifar-fs/CIFAR_FS_val.pickle", "wb"))
+pickle.dump(pickle_dict, open("materials/cifar-fs/CIFAR_FS_test.pickle", "wb"))
+pickle.dump(pickle_dict, open("materials/cifar-fs/CIFAR_FS_train.pickle", "wb"))
+pickle.dump(pickle_dict, open("materials/cifar-fs/CIFAR_FS_val.pickle", "wb"))
